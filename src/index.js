d420a7764c7152f3a9f43453f68687268b575212
jeo19 <terrakkk@outlook.com>
2020-04-26 23:36:20 +0800

    refactor:Refactoring to a good looking

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import { createStore } from 'redux';
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
+number.innerHTML = 0;
+const ADD = 'ADD';
+const MINUS = 'MINUS';
 const countModifier = (count = 0, action) => {
-    if (action.type === 'ADD') {
-        return count + 1;
-    } else if (action.type === 'MINUS') {
-        return count - 1;
-    } else {
-        return count;
+    switch (action.type) {
+        case ADD:
+            return count + 1;
+        case MINUS:
+            return count - 1;
+        default:
+            return count;
     }
 };
 
@@ -18,10 +22,10 @@ const onChange = () => {
 };
 countStore.subscribe(onChange);
 const handleAdd = () => {
-    countStore.dispatch({ type: 'ADD' });
+    countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-    countStore.dispatch({ type: 'MINUS' });
+    countStore.dispatch({ type: MINUS });
 };
 add.addEventListener('click', handleAdd);
 minus.addEventListener('click', handleMinus);
