ab48fc1e0ead5bf17e064cc970b3bdf4d30d5867
jeo19 <terrakkk@outlook.com>
2020-04-30 00:18:06 +0800

    add:Setup the react-router-dom module

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Home from '../routes/Home';
+import Detail from '../routes/Detail';
 function App() {
-    return 'app';
+    return (
+        <Router>
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/:id" exact component={Detail}></Route>
+        </Router>
+    );
 }
 export default App;
